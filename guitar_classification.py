b856a67880fc474da5e482b793861f1dae0b0ba4
Jake Pelrah <pelrahjsp@gmail.com>
2024-01-21 23:36:49 -0500

    init

--- guitar_classification.py
+++ guitar_classification.py
@@ -6,14 +6,13 @@ import pathlib
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-
 import pathlib
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file(
-    'flower_photos.tar', origin=dataset_url, extract=True)
+
 
 data_dir = pathlib.Path('/home/jake/.keras/datasets/guitar_photos')
 image_count = len(list(data_dir.glob('*/*.png')))
+
+print(image_count)
 
 
 batch_size = 32
@@ -95,42 +94,19 @@ history = model.fit(
     epochs=epochs
 )
 
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
-
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-
-# epochs_range = range(epochs)
-
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
-
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
 
 
 
+img = tf.keras.utils.load_img(
+'/home/jake/egil-classification/test_images/fly.png', target_size=(img_height, img_width))
+print(img)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-# # img = tf.keras.utils.load_img(
-# # '/home/jake/Desktop/fly.png', target_size=(img_height, img_width)
-# # )
-# # print(img)
-# # img_array = tf.keras.utils.img_to_array(img)
-# # img_array = tf.expand_dims(img_array, 0) # Create a batch
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
 
-# # predictions = model.predict(img_array)
-# # score = tf.nn.softmax(predictions[0])
-
-# # print(
-# #     "This image most likely belongs to {} with a {:.2f} percent confidence."
-# #     .format(class_names[np.argmax(score)], 100 * np.max(score))
-# # )
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
